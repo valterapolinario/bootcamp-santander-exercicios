bc06b58ca87e01e6a7b74e77072a136d97ce8b76
Valter Apolinario <valter_123@hotmail.com>
2024-05-22 19:13:50 -0300

    implementado desafio de banco digital

---
+++ banco-digital/src/main/java/com/bancodigi/Endereco.java
@@ -0,0 +1,14 @@
+package com.bancodigi;
+
+import lombok.AllArgsConstructor;
+
+
+
+
+@AllArgsConstructor
+public class Endereco {
+    private String endereco;
+    private String bairro;
+    private String cidade;
+    private String uf;
+}
---
+++ banco-digital/src/main/java/com/bancodigi/IConta.java
@@ -0,0 +1,8 @@
+package com.bancodigi;
+
+public interface IConta {
+    void sacar(double valor);
+    void depositar(double valor);
+    void transferir(double valor, Conta contaDestino);
+    void imprimirExtrato();
+}
